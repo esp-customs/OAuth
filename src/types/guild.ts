172bdc834f1dc132046baf2936c554bb07f8ef98
SrGobi <alejandrosanjuangallegos@gmail.com>
2024-01-08 20:22:18 +0100

    1.1.2

--- src/types/guild.ts
+++ src/types/guild.ts
@@ -7,6 +7,8 @@ const permissionConstants = {
   0x20: 'MANAGE_GUILD',
   0x40: 'ADD_REACTION',
   0x80: 'VIEW_AUDIT_LOG',
+  0x100: 'PRIORITY_SPEAKER',
+  0x200: 'STREAM',
   0x400: 'VIEW_CHANNEL',
   0x800: 'SEND_MESSAGES',
   0x1000: 'SEND_TTS_MESSAGES',
@@ -16,15 +18,36 @@ const permissionConstants = {
   0x10000: 'READ_MESSAGES_HISTORY',
   0x20000: 'MENTION_EVERYONE',
   0x40000: 'USE_EXTERNAL_EMOJIS',
+  0x80000: 'VIEW_GUILD_INSIGHTS',
   0x100000: 'CONNECT',
   0x200000: 'SPEAK',
   0x400000: 'MUTE_MEMBERS',
-  0x800000: 'MANAGE_NICKNAMES',
-  0x1000000: 'MANAGE_ROLES',
-  0x2000000: 'MANAGE_WEBHOOKS',
-  0x4000000: 'MANAGE_EMOJIS'
+  0x800000: 'DEAFEN_MEMBERS',
+  0x1000000: 'MOVE_MEMBERS',
+  0x2000000: 'USE_VAD',
+  0x4000000: 'CHANGE_NICKNAME',
+  0x8000000: 'MANAGE_NICKNAMES',
+  0x10000000: 'MANAGE_ROLES',
+  0x20000000: 'MANAGE_WEBHOOKS',
+  0x40000000: 'MANAGE_GUILD_EXPRESSIONS',
+  0x80000000: 'USE_APPLICATION_COMMANDS',
+  0x100000000: 'REQUEST_TO_SPEAK',
+  0x200000000: 'MANAGE_EVENTS',
+  0x400000000: 'MANAGE_THREADS',
+  0x800000000: 'CREATE_PUBLIC_THREADS',
+  0x1000000000: 'CREATE_PRIVATE_THREADS',
+  0x2000000000: 'USE_EXTERNAL_STICKERS',
+  0x4000000000: 'SEND_MESSAGES_IN_THREADS',
+  0x8000000000: 'USE_EMBEDDED_ACTIVITIES',
+  0x10000000000: 'MODERATE_MEMBERS',
+  0x20000000000: 'VIEW_CREATOR_MONETIZATION_ANALYTICS',
+  0x40000000000: 'USE_SOUNDBOARD',
+  0x80000000000: 'CREATE_GUILD_EXPRESSIONS',
+  0x100000000000: 'CREATE_EVENTS',
+  0x200000000000: 'USE_EXTERNAL_SOUNDS',
+  0x400000000000: 'SEND_VOICE_MESSAGES',
 };
-
+import { ImageURLOptions } from '../rest/index';
 export default class Guild {
   /** El ID de Discord único del servidor. */
   readonly id: string;
@@ -71,10 +94,11 @@ export default class Guild {
    * Devuelve una URL al icono del servidor.
    * @param size El tamaño del icono en píxeles. (Predeterminado a 512)
    */
-  iconUrl(size = 512): string {
+  iconUrl(options: ImageURLOptions = { size: 512 }): string {
+    const extension = (this.iconHash?.startsWith('a_') && options.forceStatic) ? 'gif' : 'webp';
     return this.iconHash
       ? `https://cdn.discordapp.com/icons/${this.id}/${this.iconHash}.${
-          this.iconHash.startsWith('a_') ? 'gif' : 'png'}?size=${size}`
+      this.iconHash.startsWith('a_') ? extension : 'webp'}?size=${options.size}`
       : 'https://i.imgur.com/rdvO6lD.png';
   }
 }
