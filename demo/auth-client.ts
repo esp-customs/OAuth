03c5810372f5348753e0d1472d33b2cb6ea06ab4
SrGobi <alejandrosanjuangallegos@gmail.com>
2025-04-01 14:22:20 +0200

    chore: eliminar documentación y demo para mantener solo el código fuente en main

--- demo/auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from '../lib/client.js';
-
-export const auth = new Client({
-  id: `${process.env.CLIENT_ID || "773617743345483826"}`,
-  secret: `${process.env.CLIENT_SECRET || "uRr2MkXLZhObJ30O6durf7aiROnAkJkb"}`,
-  redirectURI: 'http://localhost:3000/auth',
-  scopes: ['identify', 'guilds', 'guilds.join']
-});
